fd3359cfd904c6f74c78ccbd1f08e8bfcefd62f7
Yuriy Korol as shra <sshra@mail.ru>
2025-09-07 20:36:12 +0500

    Lesson commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,9 @@ fun main() {
     val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello " + name + "!")
+    println("This is updated file !")
+    println("Hello, " + name + "!")
+
 
     for (i in 1..5) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
