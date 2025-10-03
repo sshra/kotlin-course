0c74a812a305fa1a6a9af7b203a0289224b24072
Yuriy Korol as shra <sshra@mail.ru>
2025-10-03 18:36:58 +0500

    homework 07.

---
+++ src/main/kotlin/ru/stimmax/kotlincourse/lessons/lesson08/homeworks/ABitHarder.kt
@@ -0,0 +1,80 @@
+﻿package com.stimmax.ru.stimmax.kotlincourse.lessons.lesson08.homeworks
+
+fun main() {
+    println(caps("FUHIUHDIUFH FHOHO oifdoi OIihfoi Ohfo "))
+
+    val mySpy = Spy("To be or not to be")
+    println(mySpy.encrypt())
+    println(mySpy.decrypt())
+
+    tableMul(10, 15)
+}
+
+// 7. Все слова с большой буквы
+fun caps(phrase: String): String {
+
+    var result = ""
+    var lastCharIsLetter = false
+    for (char in phrase.lowercase().toCharArray()) {
+        when (char) {
+            in 'a' .. 'z', in 'а' .. 'я' -> {
+                if (lastCharIsLetter) {
+                    result += char
+                } else {
+                    result += char.uppercase()
+                }
+                lastCharIsLetter = true
+            }
+            else -> {
+                result += char
+                lastCharIsLetter = false
+            }
+        }
+    }
+
+    return result
+}
+
+// 8. Игра в разведчика
+class Spy(var s: String) {
+    init {
+        if (s.length % 2 == 1) {
+            s += " "
+        }
+    }
+
+    fun encrypt(): String {
+        var result = ""
+        for (i in 0 .. s.length - 1 step 2) {
+            val sub = s.substring(i, i + 2)
+            result += sub.substring(1, 2) + sub.substring(0, 1)
+        }
+        s = result
+        return s
+    }
+
+    fun decrypt(): String {
+        s = encrypt()
+        return s
+    }
+}
+
+// 9. Таблица умножения
+fun tableMul(a: Int, b: Int) {
+    val size = (a * b).toString().length + 1
+
+    print("".padStart(size))
+    for (j in 1 .. b) {
+        print(j.toString().padStart(size))
+    }
+    println()
+
+    for (i in 1 .. a) {
+        print( i.toString().padStart(size))
+
+        for (j in 1 .. b) {
+            print( (i * j).toString().padStart(size))
+        }
+        println()
+    }
+}
---
+++ src/main/kotlin/ru/stimmax/kotlincourse/lessons/lesson08/homeworks/Homework.kt
@@ -0,0 +1,82 @@
+﻿package com.stimmax.ru.stimmax.kotlincourse.lessons.lesson08.homeworks
+
+fun main() {
+    val testFrases = arrayOf(
+        "Это невозможно выполнить за один день",
+        "Я не уверен в успехе этого проекта",
+        "Произошла катастрофа на сервере",
+        "Этот код работает без проблем",
+        "Удача"
+    );
+
+    for (test in testFrases) {
+        println(stringTransform(test));
+    }
+
+    dateExtraction("Пользователь вошел в систему -> 2021-12-01 09:48:23")
+
+    println(getAbbreviation("In my humble opinion"))
+}
+
+// 1. Преобразование строк
+fun stringTransform(s: String): String {
+    var result = s
+        .replace("невозможно", "совершенно точно возможно, просто требует времени")
+        .replace("катастрофа", "интересное событие");
+
+    if (result.startsWith("Я не уверен")) {
+        result += ", но моя интуиция говорит об обратном"
+    }
+
+    val withoutProblems = "без проблем"
+    if (result.endsWith(withoutProblems)) {
+        result = result.substring(0, result.length - withoutProblems.length) + "с парой интересных вызовов на пути"
+    }
+
+    if (s.split(" ").size == 1) {
+        result = "Иногда," + s + ", но не всегда"
+    }
+
+    return result
+}
+
+// 2. Извлечение даты из строки лога
+fun dateExtraction(log: String) {
+    val datePattern = Regex("""\d{4}-\d{2}-\d{2}""")
+    val timePattern = Regex("""\d{2}:\d{2}:\d{2}""")
+
+    try {
+        println(
+            "Date: ${datePattern.findAll(log).first().groupValues[0]}  Time: ${
+                timePattern.findAll(log).first().groupValues[0]
+            }"
+        )
+    } catch (e: Exception) {
+        println ("Date-time record is not found!")
+    }
+}
+
+// 3. Маскирование личных данных
+fun maskCreditCard(card: String): String {
+    return "**** **** **** " + card.split(" ")[3]
+}
+
+// 4. Форматирование адреса электронной почты
+fun formatEmail(mail: String): String {
+    return mail.replace("@", " [at] ").replace(".", " [dot] ")
+}
+
+// 5. Извлечение имени файла из пути.
+fun extractFileName(path: String): String {
+    return path.split("/").last()
+}
+
+// 6. Создание аббревиатуры из фразы.
+fun getAbbreviation(phrase: String): String {
+    var result = ""
+    for (word in phrase.split(" ")) {
+        result += word[0]
+    }
+
+    return result.uppercase()
+}
